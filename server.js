0caac49acc69395f7634046420f39cb661cc0a20
tohid_d.milan@yahoo.com <tohid_d.milan@yahoo.com>
2020-10-13 10:35:17 -0600

    adds new routes to server to respond to

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ app.get("/", function (req, res) {
     "<h1>Server is running.</h1><p>This server is running with Express</p>"
   );
 });
+app.get("/contact", function (req, res) {
+  res.send("tohid_d.milan@yahoo.com");
+});
+app.get("/about", function (req, res) {
+  res.send(
+    "<p>I'm a new web developer. I started my work with learning front-end and I'm developing my skills now. I'm also getting into back-end to see what's going on there.</p>"
+  );
+});
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
