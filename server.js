e7b3beab63dd04ba4f449343e24d62e105497e66
tohid_d.milan@yahoo.com <tohid_d.milan@yahoo.com>
2020-10-13 09:38:51 -0600

    adds code to make server respond to request

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 //jshint esversion:6//
 const express = require("express");
 const app = express();
+app.get("/", function (req, res) {
+  console.log(req);
+  res.send(
+    "<h1>Server is running.</h1><p>This server is running with Express</p>"
+  );
+});
 app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
